aaead50e88594eda35a4c33672667e5f417b9db5
souma9830 <prabirkumarshee8@gmail.com>
2026-03-27 01:33:57 +0530

    make markateplace section

--- src/features/Admin/services/admin.service.js
+++ src/features/Admin/services/admin.service.js
@@ -5,32 +5,49 @@ const api = axios.create({
     withCredentials: true,
 })
 
-export async function getAllDustbins(){
+export async function getAllDustbins() {
     const res = await api.get("/iot/dustbins");
     return res.data;
 }
 
-export async function getTotalWeastDeposited(){
+export async function getTotalWeastDeposited() {
     const res = await api.get("/analytics/total");
     return res.data;
 }
 
-export async function getAllStudents(){
+export async function getAllStudents() {
     const res = await api.get("/admin/students");
     return res.data;
 }
 
-export async function getOverallWeeklyAnalytics(){
+export async function getOverallWeeklyAnalytics() {
     const res = await api.get("/analytics/weekly/college");
     return res.data;
 }
 
-export async function leaderboard(){
+export async function leaderboard() {
     const res = await api.get("/leaderboard");
     return res.data;
 }
 
-export async function createDustbin(name,capacity){
+export async function createDustbin(name, capacity) {
     const res = await api.post("/iot/dustbin/create", { name, capacity });
+    return res.data;
+}
+export async function getMarketProducts() {
+    const res = await api.get("/products");
+    return res.data;
+}
+
+export async function createProduct(name, price, image) {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("price", price);
+    formData.append("image", image);
+    const res = await api.post("/admin/products", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
     return res.data;
 }
