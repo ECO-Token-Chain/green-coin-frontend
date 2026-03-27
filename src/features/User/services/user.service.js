fb6e81e162a77b22ed577e6cd706d21211504e47
Subham777-max <subhamray865@gmail.com>
2026-03-27 07:39:45 +0530

    useUser hook is created

---
+++ src/features/User/services/user.service.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:3000/api",
+    withCredentials: true,
+})
+
+export async function getWalletBalance() {
+    const res = await api.get("/user/balance");
+    return res.data;
+}
+
+export async function getProductsById(id){
+    const res = await api.get(`/products/${id}`);
+    return res.data;
+}
+
+export async function getMyweeklyAnalytics(){
+    const res = await api.get("/analytics/weekly/my");
+    return res.data;
+}
